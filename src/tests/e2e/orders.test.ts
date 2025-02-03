8bba1a9992cbcee7d6432041b826992b11738125
Miguel A. Gómez <software.crafters.sl@gmail.com>
2025-02-03 19:48:36 +0000

    green: lists no orders when store is empty

--- src/tests/e2e/orders.test.ts
+++ src/tests/e2e/orders.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { createServer } from '../../app';
 import { Server } from 'http';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 dotenv.config({
     path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
 });
@@ -14,7 +15,12 @@ describe('POST /orders', () => {
         server = await createServer(3002, dbUrl);
     });
 
+    afterEach(async () => {
+        await mongoose.connection.dropDatabase();
+    });
+
     afterAll(async () => {
+        await mongoose.connection.close();
         await server.close();
     });
 
@@ -49,5 +55,31 @@ describe('POST /orders', () => {
 
         expect(response.status).toBe(400); 
         expect(response.text).toBe('The order must have at least one item');
+    });
+});
+
+describe('GET /orders', () => {
+    let server: Server;
+    
+    beforeAll(async () => {
+        const dbUrl = process.env.MONGODB_URI as string;
+        server = await createServer(3002, dbUrl);
+    });
+
+    afterEach(async () => {
+        await mongoose.connection.dropDatabase();
+    });
+
+    afterAll(async () => {
+        await mongoose.connection.close();
+        await server.close();
+    });
+
+    it('lists no orders when store is empty', async () => {
+        const response = await request(server)
+            .get('/orders');
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual([]);
     });
 });
